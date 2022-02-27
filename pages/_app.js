3406d7ac535c49f58a1bd93c04ca974ccfcbdfe0
Kekelberi <alexei.gaponov@gmail.com>
2022-02-27 20:18:18 +0300

    save

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import { Provider } from 'react-redux'
+import store from '../src/Redux/Store'
 import '../styles/globals.css'
 
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp
